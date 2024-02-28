99ea067f4ed88c0ec4d94341487ada20ff79b1e0
Zannbott9728 <130833885+Zannbott9728@users.noreply.github.com>
2024-02-28 23:18:43 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,51 @@
+const nav = document.querySelector(".nav"),
+  searchIcon = document.querySelector("#searchIcon"),
+  navOpenBtn = document.querySelector(".navOpenBtn"),
+  navCloseBtn = document.querySelector(".navCloseBtn");
+
+searchIcon.addEventListener("click", () => {
+  nav.classList.toggle("openSearch");
+  nav.classList.remove("openNav");
+  if (nav.classList.contains("openSearch")) {
+    return searchIcon.classList.replace("uil-search", "uil-times");
+  }
+  searchIcon.classList.replace("uil-times", "uil-search");
+});
+
+navOpenBtn.addEventListener("click", () => {
+  nav.classList.add("openNav");
+  nav.classList.remove("openSearch");
+  searchIcon.classList.replace("uil-times", "uil-search");
+});
+navCloseBtn.addEventListener("click", () => {
+  nav.classList.remove("openNav");
+});
+//////////)////////////
+let slideIndex = 0;
+showSlide(slideIndex);
+
+function nextSlide() {
+    showSlide(slideIndex += 1);
+}
+
+function prevSlide() {
+    showSlide(slideIndex -= 1);
+}
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.slider img');
+    
+    if (index >= slides.length) {
+        slideIndex = 0;
+    } else if (index < 0) {
+        slideIndex = slides.length - 1;
+    }
+    
+    slides.forEach(function(slide) {
+        slide.style.display = 'none';
+    });
+    
+    slides[slideIndex].style.display = 'block';
+}
+////////////
+
